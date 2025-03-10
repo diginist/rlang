fa5418fa2e9ea0a655d8ae7cbaa11f6566c3f747
infernostars <infernity@infernity.dev>
2025-03-09 20:45:29 -0600

    i should add this as a flag. eventually

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def repl(inp: Interpreter):
     while True:
         try:
             torun = input("\033[94;1m> \033[0;90m")
-            error = inp.run_line(torun)
+            error = inp.run_line_wrapped(torun)
             if error:
                 print(error)
             print(inp.out_stream, end="")
